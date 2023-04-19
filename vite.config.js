aed0c01b49f1c9467784faa5289def9e55020102
litong <programingwithli@gmail.com>
2023-04-19 17:18:12 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+// Plugins
+import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import AutoImport from 'unplugin-auto-import/vite'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia']
+    })
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
+  },
+  server: {
+    port: 3000
+  },
+  css: {
+    preprocessorOptions: {
+      scss: { charset: false },
+      css: { charset: false }
+    }
+  }
+})
