58834859a53445f9e8df7f6054a93ccd11b17829
litong <programingwithli@gmail.com>
2023-05-19 16:34:24 +0800

    feat: new feature and fixed style
    - use react in vue component
    - add online codesandbox IDE
    - common-layout height

--- src/views/playground/Playground.vue
+++ src/views/playground/Playground.vue
@@ -1,11 +1,54 @@
 <template>
-  <div>
-    playground
+  <div class="py-4" color="background" ref="sandbox" style="height: calc(100%)">
+    <div class="mb-4 d-flex justify-space-between">
+      <v-btn>Toggle Language</v-btn>
+      <v-btn
+        @click="toggle"
+        color="primary"
+        :icon="isFullscreen ? 'mdi-fullscreen-exit' : 'mdi-fullscreen'"
+      ></v-btn>
+    </div>
+    <CodeSandbox :themeMode="theme"/>
   </div>
 </template>
 
-<script setup name="Playground">
+<script>
+import { ref } from 'vue'
+import { applyPureReactInVue } from 'veaury'
+import { mapState } from 'pinia';
+import { useFullscreen } from '@vueuse/core'
 
+import { useConfigureStore } from '@/store/configure';
+import CodeSandbox from './components/react_app/CodeSandbox'
+
+export default {
+  name: 'Playground',
+  components: {
+    CodeSandbox: applyPureReactInVue(CodeSandbox),
+  },
+  data() {
+    return {
+    }
+  },
+  setup() {
+    const sandbox = ref(null)
+    const { isFullscreen, toggle } = useFullscreen(sandbox)
+
+    return {
+      sandbox,
+      isFullscreen,
+      toggle
+    }
+  },
+  computed: {
+    ...mapState(useConfigureStore, ['theme'])
+  },
+  methods: {
+    handleFullScreen() {
+      this.isFullScreen = !this.isFullScreen
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
+// import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import AutoImport from 'unplugin-auto-import/vite'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
+import veauryVitePlugins from 'veaury/vite/index.js'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -11,8 +12,14 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: { transformAssetUrls }
+    // vue({
+    //   template: { transformAssetUrls }
+    // }),
+    veauryVitePlugins({
+      type: 'vue',
+      vueOptions: {
+        template: { transformAssetUrls }
+      }
     }),
     vueSetupExtend(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
